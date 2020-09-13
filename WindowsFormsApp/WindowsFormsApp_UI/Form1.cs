9cdc83c5ee87dca067a4aadbe2e9224ac5fe0837
ando.yuma <foymitsu-1115pye@outlook.jp>
2020-09-13 20:47:22 +0900

    Block Practice.

--- WindowsFormsApp/WindowsFormsApp_UI/Form1.cs
+++ WindowsFormsApp/WindowsFormsApp_UI/Form1.cs
@@ -1,21 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
+
+/// <summary>
+/// 名前空間
+/// </summary>
 namespace WindowsFormsApp_UI
 {
+    /// <summary>
+    /// クラス
+    /// </summary>
     public partial class Form1 : Form
     {
+        private int _value = 0;
         public Form1()
         {
             InitializeComponent();
-         
+
+            _value = 2;
+           
+        }
+
+        private int GetValue()
+        {
+            int valueM = 0;
+            _value = 3;
+            valueM = 5;
+            return 1;
+        }
+
+        private void SetValue()
+        {
+            MessageBox.Show("Hello World!");
         }
     }
 }
