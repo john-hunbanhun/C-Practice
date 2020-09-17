61536dc494fc578478891159d34a279143b0322c
ando.yuma <foymitsu-1115pye@outlook.jp>
2020-09-17 21:12:58 +0900

    Customer_Class追加

---
+++ WindowsFormsApp/WindowsFormsApp_UI/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp_UI
+{
+    class Customer
+    {
+        /// <summary>
+        /// 顧客クラス
+        /// </summary>
+        public string FirstName;
+        public string LastName;
+
+        public string GetFullName()
+        {
+            return LastName + FirstName;
+        }
+    }
+}
---
+++ WindowsFormsApp/WindowsFormsApp_UI/Form4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_UI
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Customer customer = new Customer();
+            customer.LastName = "谷澤";
+            customer.FirstName = "恵里香";
+
+            MessageBox.Show(customer.GetFullName());
+        }
+    }
+}
