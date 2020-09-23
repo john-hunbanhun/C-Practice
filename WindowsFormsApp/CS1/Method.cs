7b3cb434dc6e77332488524abe3f2d791a4fc486
ando.yuma <foymitsu-1115pye@outlook.jp>
2020-09-23 20:49:15 +0900

    Method/オーバーロードの追加

---
+++ WindowsFormsApp/CS1/Method.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS1
+{
+    class Method
+    {
+        public void CheckData(int value , int value2)
+        {
+            if (value == 1)
+            {
+
+            }
+        }
+
+        public int GetCount()
+        {
+            return 1;
+        }
+
+        public void Save(int id,string name)
+        {
+            //save処理
+        }
+        public void Save(string name ,int id)
+        {
+            //オーバーロード　Save処理
+        }
+    }
+}
