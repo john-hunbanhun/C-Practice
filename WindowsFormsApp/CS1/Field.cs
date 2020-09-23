f9500fae75348acebc62cbbf6afd9b9d822d647c
ando.yuma <foymitsu-1115pye@outlook.jp>
2020-09-23 20:57:45 +0900

    Fieldの追加

---
+++ WindowsFormsApp/CS1/Field.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS1
+{
+    public class Field
+    {
+        private int _count = 0; //フィールド名は頭に_を付けるのが慣例(命名規約)
+
+        public void Call()
+        {
+            int count = 0;
+
+            _count++;
+        }
+    }
+}
